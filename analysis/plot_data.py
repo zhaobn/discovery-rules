427ef3091df55e27335cc73576b2e8ed897a6d2f
bonan <zbn.dale@gmail.com>
2025-01-02 17:55:51 +0000

    pilot data

---
+++ analysis/plot_data.py
@@ -0,0 +1,20 @@
+# %%
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+# %%
+subject_data = pd.read_csv("../data/subject_data.csv")
+subject_data['assignment'] = pd.Categorical(subject_data['assignment'], categories=['easy', 'medium', 'hard'], ordered=True)
+
+plt.figure(figsize=(12, 6))
+sns.scatterplot(data=subject_data, x='assignment', y='total_points', hue='assignment', style='assignment', s=100)
+plt.yscale('log')
+plt.title('Total Points per Condition (Log Scale)')
+plt.xlabel('Assignment')
+plt.ylabel('Total Points (Log Scale)')
+plt.legend(title='Assignment')
+plt.show()
+
+# %%
